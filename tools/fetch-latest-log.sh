8d20b04423928bf107167c902740c15871b663ce
SkinnyBonesArv <skinnybonesarv@gmail.com>
2019-01-22 20:37:04 -0500

    Fix log fetch tool

--- tools/fetch-latest-log.sh
+++ tools/fetch-latest-log.sh
@@ -1,5 +1,5 @@
-$server = "lvuser@172.22.11.2"
-$serverDir = "~/log/"
-$clientDir = "C:/Users/BUZZ-175/RoboRIOLogs/"
+server="lvuser@172.22.11.2"
+serverDir="/home/lvuser/log"
+clientDir="C:/Users/BUZZ-175/RoboRIOLogs/"
 
-scp $server:$serverDir/$(ssh $server 'ls -t $serverDir | head -1') $clientDir
+scp $server:$serverDir/$(ssh $server 'ls -t /home/lvuser/log | head -1') $clientDir
