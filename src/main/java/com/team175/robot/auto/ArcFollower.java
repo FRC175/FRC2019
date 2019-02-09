7297f9eb4e42cc704c30ab1f06bfa4139d15985d
SkinnyBonesArv <SkinnyBonesArv@Gmail.com>
2019-02-09 10:48:02 -0500

    Begin motion profiling code

---
+++ src/main/java/com/team175/robot/auto/ArcFollower.java
@@ -0,0 +1,10 @@
+package com.team175.robot.auto;
+
+import com.ctre.phoenix.motion.BufferedTrajectoryPointStream;
+
+public class ArcFollower {
+
+    private BufferedTrajectoryPointStream mPointWriter;
+
+
+}
